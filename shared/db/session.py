eb86a9ced9cfa60ba7146dcb6f0d6048cef61703
Felippe Martan <kanas3600@outlook.com>
2026-05-24 18:53:25 -0300

    Music-Library-API

---
+++ shared/db/session.py
@@ -0,0 +1,8 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
